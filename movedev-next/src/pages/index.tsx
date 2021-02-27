9ea1f85a076092c46de781a6bfda189771efa753
Andréa Soares <asoaresdev@gmail.com>
2021-02-27 15:05:52 -0300

    feat: quinto dia

--- movedev-next/src/pages/index.tsx
+++ movedev-next/src/pages/index.tsx
@@ -1,15 +1,30 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenge } from '../components/CompletedChalenge';
 import { Countdown } from '../components/Countdown';
 import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
-
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 import styles from '../styles/pages/Home.module.css'
-export default function Home() {
+
+interface HomeProps {
+  level:number, 
+  currentExperience:number, 
+  challengesCompleted:number 
+}
+
+export default function Home(props:HomeProps) {
+  console.log(props);
+  
   return (
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
 
     <Head>
@@ -30,5 +45,19 @@ export default function Home() {
       </section>
     </CountdownProvider>
    </div>
+   </ChallengesProvider>
   )
+}
+
+export const getServerSideProps:GetServerSideProps  = async(ctx) => {
+  // const cookies = ctx.req.cookies
+  //desestruturando
+  const {level, currentExperience, challengesCompleted } = ctx.req.cookies
+  return {
+    props:{
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted) 
+    }
+  }
 }
